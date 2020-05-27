dfa1afc6e6f9bfe30958882348a1720e8f9ffc31
FODIL zine-eddine <hz_fodil@esi.dz>
2020-05-27 10:32:03 +0100

    ajout de la méthode de connexion avec email et une page home vers lequel on va rediriger si les données son correctes

--- lib/Classes/SignIn.dart
+++ lib/Classes/SignIn.dart
@@ -1,5 +1,7 @@
 
+import 'package:auth_and_sign_in/Classes/home.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,6 +10,25 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String _email,_password;
+  final GlobalKey<FormState> _formKey= GlobalKey<FormState>();
+
+  //une méthode async pour le login
+  Future<void> signIn() async {
+    //validation du fomulaire
+    if(_formKey.currentState.validate()){
+      _formKey.currentState.save();
+      //acceder à firebase
+      try{
+        AuthResult result = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password); 
+        //connection , si les données ne sont pas correctes , elle va lever une exception
+        Navigator.push(context, MaterialPageRoute(builder: (context)=> Home(user: result.user,)));
+      }catch(e){
+        print(e.message);
+      }
+    }
+    
+  }
+
   @override
   Widget build(BuildContext context) {
   
@@ -16,9 +37,39 @@ class _LoginPageState extends State<LoginPage> {
         title: Text('Sign In'),
       ),
       body: Form(
+        key: _formKey,
         child:Column(
           children: <Widget>[
+            TextFormField(
+              validator: (input){
+                if(input.isEmpty){
+                  return 'Veuillez introduire votre Email';
+                }
+                return null;
+              },
+              onSaved: (input)=> _email=input,
+              decoration: InputDecoration(
+                labelText: 'Email',
 
+              ),
+            ),
+            TextFormField(
+              validator: (input){
+                if(input.isEmpty){
+                  return 'Veuillez introduire votre Mot de passe ';
+                }
+                return null;
+              },
+              onSaved: (input)=> _password=input,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            RaisedButton(
+              onPressed: signIn,
+             child: Text('Sign in'),
+            ),
           ],
         ),
       ),
---
+++ lib/Classes/home.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  final FirebaseUser user;
+
+  const Home({Key key, this.user}) : super(key: key);
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home page')),
+      body: Center(
+        child:Text('Welcome to you ${widget.user.email}'),
+      ),
+    );
+  }
+}
