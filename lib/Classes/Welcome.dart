735a9d956f3476a4362a85adc7205ca6e2f21e1f
FODIL zine-eddine <hz_fodil@esi.dz>
2020-05-30 21:17:57 +0100

    ajout du design pour Welcome Page

--- lib/Classes/Welcome.dart
+++ lib/Classes/Welcome.dart
@@ -10,22 +10,59 @@ class WelcomePage extends StatefulWidget {
 class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-          appBar: AppBar(
-            title: Text("Welcome to CovApp"),
-          ),
-          body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          FlatButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginPage(),fullscreenDialog: true,));
-          }, child: Text("Sign In")
-          ),
-          FlatButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=> SignUpPage(),fullscreenDialog: true,));
-          }, child: Text("Sign Up"))
-        ],
+    return Container(
+      decoration: BoxDecoration(
+        image:  DecorationImage(
+          image: AssetImage("images/WelcomePage.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Welcome to CovApp"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginPage(),
+                      fullscreenDialog: true,
+                    ));
+              },
+              child: Text("Sign In"),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                  side: BorderSide(
+                    color: Colors.white,
+                  )),
+              color: Colors.transparent,
+              textColor: Colors.white,
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SignUpPage(),
+                      fullscreenDialog: true,
+                    ));
+              },
+              child: Text("Sign Up"),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                  side: BorderSide(
+                    color: Colors.blue[300],
+                  )),
+              color: Colors.blue[300],
+              textColor: Colors.white,
+            )
+          ],
+        ),
       ),
     );
   }
